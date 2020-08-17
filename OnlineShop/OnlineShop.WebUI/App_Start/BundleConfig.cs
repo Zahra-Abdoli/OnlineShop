93ce7d25a4ba1ed16e83f9de351811108adfe435
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-08-17 12:29:18 +0200

    remove extra line

--- OnlineShop/OnlineShop.WebUI/App_Start/BundleConfig.cs
+++ OnlineShop/OnlineShop.WebUI/App_Start/BundleConfig.cs
@@ -3,7 +3,6 @@ using System.Web.Optimization;
 
 namespace OnlineShop.WebUI
 {
-
     public class BundleConfig
     {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
