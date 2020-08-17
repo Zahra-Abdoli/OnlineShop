821556a1396af404d893c4f0073722ec8d10c3ce
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-08-17 12:22:03 +0200

    first seup

---
+++ OnlineShop/OnlineShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineShop.WebUI.Startup))]
+namespace OnlineShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
