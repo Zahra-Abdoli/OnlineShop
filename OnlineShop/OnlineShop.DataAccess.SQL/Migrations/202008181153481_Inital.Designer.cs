61e34a6e06670eefc1bce285014f97eab60a2b97
Greeshma Sadineni <greeshma.sadineni@gmail.com>
2020-08-18 14:47:17 +0200

    Done With Migration and views for product admin and category admin Pages

--- OnlineShop/OnlineShop.DataAccess.SQL/Migrations/202008181153481_Inital.Designer.cs
+++ OnlineShop/OnlineShop.DataAccess.SQL/Migrations/202008181153481_Inital.Designer.cs
@@ -7,13 +7,13 @@ namespace OnlineShop.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class Initial : IMigrationMetadata
+    public sealed partial class Inital : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Inital));
         
         string IMigrationMetadata.Id
         {
-            get { return "202008181101566_Initial"; }
+            get { return "202008181153481_Inital"; }
         }
         
         string IMigrationMetadata.Source
