5f6ba8a78859cde7085174938fc1c7d572ff5bcd
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-08-18 10:48:51 +0200

    Set Up Unity and repository

---
+++ OnlineShop/OnlineShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShop.Core;
+
+namespace OnlineShop.DataAccess.SQL
+{
+        public class DataContext : DbContext
+        {
+            public DataContext()
+                : base("DefaultConnection")
+            {
+
+            }
+            public DbSet<Product> Products { get; set; }
+            public DbSet<Category> ProductCategories { get; set; }
+          
+    }
+}
